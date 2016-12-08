e8e063f561d43f86fc946a4d7cec0817486750be
Zeke <zeke@sweetmojo.com>
2016-12-07 23:00:55 -0500

    Update PYX.py

--- PYX.py
+++ PYX.py
@@ -1,30 +1,29 @@
 # PYX (Python Exchange)
 # Created by Logicmn
-# 11/19/16
+# Started 11/19/16
 
-#          -----------------------------------------
-#          | Real-time stock trading program using |
-#          |   a basic mean reversion algorithm    |
-#          -----------------------------------------
+#                               -----------------------------------------
+#                               | Real-time stock trading program using |
+#                               |   a basic mean reversion algorithm    |
+#                               -----------------------------------------
 
-#--------------------------------------------------------------------------------------------------------------
+#-------------------------------------Dependencies and database link-------------------------------------------
 import datetime
 from yahoo_finance import Share
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Sequence, MetaData, create_engine
-from sqlalchemy.orm import sessionmaker, subqueryload
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///db2.db', echo=True)
+engine = create_engine('sqlite:///db2.db', echo=True) # Link the database to the SQLAlchemy engine
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 metadata = MetaData()
 session = Session()
-
 #--------------------------------------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------------------------
-class Wallet(Base):
+#-------------------------------------Creating tables for database--------------------------------------------
+class Wallet(Base): # Create 'wallets' table
     __tablename__ = 'wallets'
 
     id = Column(Integer, Sequence('wallet_id_seq'), primary_key=True)
@@ -34,7 +33,7 @@ class Wallet(Base):
     def __repr__(self):
         return "<Wallet(name='%s', balance='%s')>" % (self.name, self.balance)
 
-class Transaction(Base):
+class Transaction(Base): # Create 'transactions' table
     __tablename__ = 'transactions'
 
     id = Column(Integer, Sequence('transaction_id_seq'), primary_key=True)
@@ -51,40 +50,40 @@ class Transaction(Base):
                % (self.stock, self.symbol, self.buy_or_sell, self.price, self.ema, self.shares, self.time)
 #--------------------------------------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------------------------
-class Strategy(object):
+#----------------------------------------Creating mean reversion algorithm-------------------------------------
+class Strategy(object): # Create the algorithm PYX will use to trade with
     def __init__(self, equity):
         self.equity = equity
 
     def getEquity(self):
         return self.equity
 
-    def calcEMA(self, closePrice, prevEMA):
+    def calcEMA(self, closePrice, prevEMA): # Calculate the exponential moving average
         multiplier = 2 / (50 + 1)
         EMA = (closePrice - prevEMA) * multiplier + prevEMA
         return EMA
 
-    def calcUpper(self, EMA):
+    def calcUpper(self, EMA): # Calculate the upper Bollinger band
         upperBand = EMA * (1 + .02)
         return upperBand
 
-    def calcLower(self, EMA):
+    def calcLower(self, EMA): # Calculate the lower Bollinger band
         lowerBand = EMA * (1 - .02)
         return lowerBand
 #--------------------------------------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------------------------
-def enter_position(mean_reversion, apple):
+#------------------------------------------Buy/sell shares of a stock------------------------------------------
+def enter_position(mean_reversion, apple): # Buy shares of a stock
     closePrice = float(apple.get_prev_close())
     prevEMA = float(apple.get_50day_moving_avg())
     EMA = mean_reversion.calcEMA(closePrice, prevEMA)
     lowerBand = float(mean_reversion.calcLower(EMA))
-    purchase_query = session.query(Transaction.buy_or_sell).order_by(Transaction.id.desc()).first()
+    purchase_query = session.query(Transaction.buy_or_sell).order_by(Transaction.id.desc()).first() # Find out whether the latest purchase was a buy/sell
     if purchase_query != None:
         purchase = purchase_query[0]
     else:
         purchase = 'sell'
-    if float(apple.get_price()) <= lowerBand and purchase != 'buy':
+    if float(apple.get_price()) <= lowerBand and purchase != 'buy': # Buy shares if the last purchase was a sell
         print('buy')
         new_transaction = Transaction(stock='Apple', symbol=apple.get_info()['symbol'], buy_or_sell='buy',
                                       price=apple.get_price(),
@@ -98,23 +97,23 @@ def enter_position(mean_reversion, apple):
         current_bal = session.query(Wallet).one()
         session.delete(current_bal)
         session.commit()
-        new_bal = balance[0] - new_funds
+        new_bal = balance[0] - new_funds # Subtract amount spent from the balance in wallet
         primary_wallet = Wallet(name='Primary Wallet', balance=new_bal)
         session.add(primary_wallet)
         session.commit()
 
 
-def exit_position(mean_reversion, apple):
+def exit_position(mean_reversion, apple): # Sell shares of a stock
     closePrice = float(apple.get_prev_close())
     prevEMA = float(apple.get_50day_moving_avg())
     EMA = mean_reversion.calcEMA(closePrice, prevEMA)
     upperBand = float(mean_reversion.calcUpper(EMA))
-    purchase_query = session.query(Transaction.buy_or_sell).order_by(Transaction.id.desc()).first()
+    purchase_query = session.query(Transaction.buy_or_sell).order_by(Transaction.id.desc()).first() # Find out whether the latest purchase was a buy/sell
     if purchase_query != None:
         purchase = purchase_query[0]
     else:
         purchase = 'buy'
-    if float(apple.get_price()) >= upperBand and purchase != 'sell':
+    if float(apple.get_price()) >= upperBand and purchase != 'sell': # Sell shares if the last purchase was a buy
         print('sell')
         new_transaction = Transaction(stock='Apple', symbol=apple.get_info()['symbol'], buy_or_sell='sell',
                                       price=apple.get_price(),
@@ -128,24 +127,26 @@ def exit_position(mean_reversion, apple):
         current_bal = session.query(Wallet).one()
         session.delete(current_bal)
         session.commit()
-        new_bal = balance[0] + new_funds
+        new_bal = balance[0] + new_funds # Add amount gained to the balance in wallet
         primary_wallet = Wallet(name='Primary Wallet', balance=new_bal)
         session.add(primary_wallet)
         session.commit()
 #--------------------------------------------------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------Main function------------------------------------------------
 def main():
-    apple = Share('AAPL')
+    apple = Share('AAPL') # Which stock to monitor and invest in
     Base.metadata.create_all(engine)
     session.commit()
-    b = session.query(Wallet.balance).first()
+    b = session.query(Wallet.balance).first() # Check if there is already a wallet
     if b == None:
-        primary_wallet = Wallet(name='Primary Wallet', balance=100000)
+        primary_wallet = Wallet(name='Primary Wallet', balance=100000) # Create the wallet with a balance of $100,000
         session.add(primary_wallet)
         session.commit()
-    mean_reversion = Strategy(apple.get_info()['symbol'])
-    enter_position(mean_reversion, apple)
-    exit_position(mean_reversion, apple)
+    mean_reversion = Strategy(apple.get_info()['symbol']) # Run the EMA, and Bollinger band calculations
+    enter_position(mean_reversion, apple) # Buy stock if applicable
+    exit_position(mean_reversion, apple) # Sell stock if applicable
     session.commit()
 #--------------------------------------------------------------------------------------------------------------
+
+main()
